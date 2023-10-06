232adc056c532356ee7e8f160b352d92cf0cf0ac
Nhan Bach <>
2023-10-06 09:52:20 -0700

    tweak seed.py to work with prod database

--- seed.py
+++ seed.py
@@ -1,21 +1,38 @@
 """Seed database with sample data from CSV Files."""
 
 from csv import DictReader
-from app import db, app
+from models import db
 from models import User, Message, Follows
-
-with app.app_context():
+def seed():
 
     db.drop_all()
     db.create_all()
 
     with open('generator/users.csv') as users:
         db.session.bulk_insert_mappings(User, DictReader(users))
+    db.session.commit()
+
+    users = db.session.query(User).add_column(User.id).all()
 
     with open('generator/messages.csv') as messages:
-        db.session.bulk_insert_mappings(Message, DictReader(messages))
-
-    with open('generator/follows.csv') as follows:
-        db.session.bulk_insert_mappings(Follows, DictReader(follows))
+        messages_reader = DictReader(messages)
+        final_list = []
+        for m in messages_reader:
+            m['user_id'] = users[int(m['user_id']) - 1].id
+            final_list.append(m)
+        db.session.bulk_insert_mappings(Message, final_list)
 
     db.session.commit()
+
+    with open('generator/follows.csv') as follows:
+        follows_reader = DictReader(follows)
+        final_follows = []
+        for f in follows_reader:
+            f['user_being_followed_id'] = users[int(f['user_being_followed_id']) - 1].id
+            f['user_following_id'] = users[int(f['user_following_id']) - 1].id
+            final_follows.append(f)
+        db.session.bulk_insert_mappings(Follows, final_follows)
+
+    db.session.commit()
+    
+
